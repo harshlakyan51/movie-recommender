6cf4e692474760559210eeaa4ab525c2aa19869e
harshlakyan51 <149249720+harshlakyan51@users.noreply.github.com>
2025-07-14 12:29:09 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import pickle
+import streamlit as st
+
+#  Recommend movies
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    for i in distances[1:6]:
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+    return recommended_movie_names
+
+#  Streamlit UI
+
+st.header('🎥 Movie Recommender System')
+
+# Load data
+movies = pickle.load(open('movie_list.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+movie_list = movies['title'].values
+selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
+
+if st.button('Show Recommendation'):
+    names = recommend(selected_movie)
+    st.subheader("📋 Recommended Movies:")
+    for name in names:
+        st.write(f"🎞️ {name}")
